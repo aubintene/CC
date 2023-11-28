c21f9f672d21186d682b6f6dbdf1a825d710cac2
dell <dell@AUBIN-TENE>
2023-11-28 03:07:55 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        ListView1 = New ListView()
+        ColumnHeader1 = New ColumnHeader()
+        ColumnHeader2 = New ColumnHeader()
+        ColumnHeader3 = New ColumnHeader()
+        GroupBox2 = New GroupBox()
+        Button1 = New Button()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(ListView1)
+        GroupBox1.Location = New Point(91, 42)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(719, 286)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Liste :"
+        ' 
+        ' ListView1
+        ' 
+        ListView1.Columns.AddRange(New ColumnHeader() {ColumnHeader1, ColumnHeader2, ColumnHeader3})
+        ListView1.Location = New Point(19, 23)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(710, 257)
+        ListView1.TabIndex = 0
+        ListView1.UseCompatibleStateImageBehavior = False
+        ListView1.View = View.Details
+        ' 
+        ' ColumnHeader1
+        ' 
+        ColumnHeader1.Text = "Nom"
+        ' 
+        ' ColumnHeader2
+        ' 
+        ColumnHeader2.Text = "Type"
+        ' 
+        ' ColumnHeader3
+        ' 
+        ColumnHeader3.Text = "Immatriculation"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(Button1)
+        GroupBox2.Controls.Add(TextBox3)
+        GroupBox2.Controls.Add(TextBox2)
+        GroupBox2.Controls.Add(TextBox1)
+        GroupBox2.Controls.Add(Label3)
+        GroupBox2.Controls.Add(Label2)
+        GroupBox2.Controls.Add(Label1)
+        GroupBox2.Location = New Point(40, 360)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(314, 202)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Ajouter"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(70, 157)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 6
+        Button1.Text = "Ajouter"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(152, 102)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(106, 65)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 4
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(106, 23)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 3
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(25, 102)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(121, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Immatriculation :"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(25, 65)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(47, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Type :"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(25, 24)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(49, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Nom :"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(471, 351)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 2
+        Button2.Text = "Enregistrer"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(620, 351)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 3
+        Button3.Text = "Recharger"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1153, 649)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,116 @@
+﻿
+Public Class Form1
+    'Bouton Ajouter des éléments a la ListView
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Ajouter des éléments a la ListView
+        ListView1.Items.Add(New ListViewItem(New String() {TextBox1.Text, TextBox2.Text, TextBox3.Text}))
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+    End Sub
+
+    'Bouton Enregistrer la ListView
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Choisir l'emplacement d'enregistrement des données.
+        Using sauve As New SaveFileDialog()
+            sauve.Filter = "Fichier texte|*.txt"
+            If sauve.ShowDialog() = DialogResult.OK Then
+                SauvegarderLesDonnées(ListView1, sauve.FileName)
+            End If
+
+        End Using
+
+    End Sub
+
+    'Bouton Recharger la ListView
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Choisir le fichier a recharger dans la ListView
+        Using Ouvrir As New OpenFileDialog
+            Ouvrir.Filter = "Texte|*.txt" 'Pour plusieurs extensions on fait: Texte|*.txt|Application|*.exe|Tous|*.*
+            If Ouvrir.ShowDialog() = DialogResult.OK Then
+                RechargerLesDonnées(ListView1, Ouvrir.FileName)
+            End If
+
+        End Using
+
+    End Sub
+
+
+    'Méthode pour sauvegarder les donénes dans un fichier.
+    Sub SauvegarderLesDonnées(ByVal Liste As ListView, ByVal Fichier As String)
+
+        Dim ItemSplit As String = "(*E*)" 'Pour séparer entre les éléments -> élément1(*E*)élémen2(*E*)élément3
+        Dim ColonneSplit As String = "(*C*)" 'Pour séparer entre les valeurs -> Nom(*C*)Age(*C*)(*Couleur*)
+        Dim Build As New Text.StringBuilder() 'Un outil très utile pour construire du string
+        Dim nElement As Integer = Liste.Items.Count
+
+        For Each element As ListViewItem In Liste.Items 'Pour chaque élément dans la ListView
+
+            For i As Integer = 0 To Liste.Columns.Count - 1 'Pour chaque colonne dans un élément
+                Build.Append(element.SubItems(i).Text) 'Ajouter la valeur du colonne dans le Build
+                If (i < Liste.Columns.Count - 1) Then 'Si ce n'est pas la dernière colonne.
+                    Build.Append(ColonneSplit) 'Ajouter le séparateur (*C*)
+                End If
+            Next
+
+            Build.Append(ItemSplit) 'Séparer entre chaque élément -> David(*C*)25(*C*)Rouge(*E*)
+        Next
+
+
+
+        Try
+
+            IO.File.WriteAllText(Fichier, Build.ToString()) 'écrir le build dans le fichier 
+            MessageBox.Show("Sauvegardé")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Erreur")
+        End Try
+
+
+    End Sub
+
+    'Méthode pour rechager les donénes depuis un fichier.
+    Sub RechargerLesDonnées(ByVal Liste As ListView, ByVal Fichier As String)
+        Liste.Items.Clear()
+        Dim ItemSplit As String = "(*E*)"
+        Dim ColonneSplit As String = "(*C*)"
+
+
+        Try 'Au cas d'erreur
+            If IO.File.Exists(Fichier) Then 'Si le fichier existe
+
+                Dim LesDonnées As String = IO.File.ReadAllText(Fichier) 'Lire toute les données
+                Dim LesElements() As String = LesDonnées.Split(New String() {ItemSplit}, StringSplitOptions.RemoveEmptyEntries) 'Séparer avec (*E*)
+
+                For Each element As String In LesElements 'Pour chaque éléments dans LesElements()
+                    If (element.Contains(ColonneSplit)) Then 'Si ça contient le séparateur (*C*)
+
+                        Dim LesValeurs() As String = element.Split(New String() {ColonneSplit}, StringSplitOptions.None) 'Séparer avec (*C*)
+                        Dim NouveauElement As New ListViewItem 'Nouveau élément a ajouter dans la ListView
+
+                        For i As Integer = 0 To LesValeurs.Length - 1 'Pour chaque valeur dans  LesValeurs()
+                            If i = 0 Then 'Si c'est la première valeur
+                                NouveauElement.Text = LesValeurs(i) 'Ajouter la valeur dans la première colonne.
+                            Else 'Si non l'ajouter dans les autres colonnes
+                                NouveauElement.SubItems.Add(LesValeurs(i)) 'Si i = 1 donc c'est la deuxième colonne etc
+                            End If
+                        Next
+                        Liste.Items.Add(NouveauElement) 'Ajouter l'élément a la ListView
+
+                    End If
+                Next
+
+
+            End If
+
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Erreur")
+        End Try
+
+
+    End Sub
+
+End Class
+
